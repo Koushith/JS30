91976103e6564d3deec82c70937c3d142c1032d6
koushith <koushith97@gmail.com>
2020-05-02 23:09:33 +0530

    updated

---
+++ oops/encapusalation.js
@@ -0,0 +1,14 @@
+// grouping of related data togather.
+
+let employee = {
+  name: 'koushith',
+  base_sal: 30000,
+  shift_allowence: 5000,
+  over_time: 100,
+  monthly_wage: function () {
+    return this.base_sal + this.shift_allowence * this.over_time;
+  },
+};
+
+output = employee.monthly_wage();
+console.log(output);
