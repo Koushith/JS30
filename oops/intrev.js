493bc213066410c01ef3ad03de2d15270f0d7942
koushith <koushith97@gmail.com>
2020-05-28 17:46:07 +0530

    int rev

---
+++ oops/intrev.js
@@ -0,0 +1,10 @@
+// CHALLENGE 3: REVERSE AN INTEGER
+// Return an integer in reverse
+// ex. reverseInt(521) === 125
+
+function reverseInt(int) {
+  let reverse = int.toString().split('').reverse().join('');
+  return parseInt(reverse);
+}
+
+console.log(reverseInt(123456789));
