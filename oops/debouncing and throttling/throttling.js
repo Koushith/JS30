ce8082ff03b266f28e7a764673fd8472c8e5a5f2
koushith <koushith97@gmail.com>
2020-05-28 20:03:08 +0530

    debounce

--- oops/debouncing and throttling/throttling.js
+++ oops/debouncing and throttling/throttling.js
@@ -1,11 +1,31 @@
-const debounce = (fn, delay) => {
-  let timeoutID;
-  return function (args) {
-    if (timeoutID) {
-      clearTimeout(timeoutID);
+// const debounce = (fn, delay) => {
+//   let timeoutID;
+//   return function (args) {
+//     if (timeoutID) {
+//       clearTimeout(timeoutID);
+//     }
+//     timeoutID = setTimeout(() => {
+//       fn(args);
+//     }, delay);
+//   };
+// };
+
+// document.getElementById('btn').addEventListener(
+//   'click',
+//   debounce((e) => {
+//     console.log('clicked');
+//   }, 2000)
+// );
+
+const debounce = function (fn, delay) {
+  return function () {
+    let timeOutId;
+    if (timeOutId) {
+      clearTimeout(timeOutId);
     }
-    timeoutID = setTimeout(() => {
-      fn(args);
+
+    timeOutId = setTimeout(() => {
+      fn();
     }, delay);
   };
 };
