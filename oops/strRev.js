5f6eb6bff3a68b7ab2b8469bb55e78ca37f54af7
koushith <koushith97@gmail.com>
2020-05-28 17:20:37 +0530

    str rev

---
+++ oops/strRev.js
@@ -0,0 +1,26 @@
+// CHALLENGE 1: REVERSE A STRING
+// Return a string in reverse
+// ex. reverseString('hello') === 'olleh'
+
+// function reverseString(str) {
+//   const strArr = str.split('');
+//   strArr.reverse();
+//   return strArr.join('');
+// }
+
+// console.log(reverseString('hello'));
+// function reverseString(str) {
+//   return str.split('').reverse().join('');
+// }
+// console.log(reverseString('hello'));
+
+// function reverseString(str) {
+//   let revStr = '';
+//   str.split('').forEach((char) => (revStr = char + revStr));
+//   return revStr;
+// }
+
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+console.log(reverseString('hello'));
