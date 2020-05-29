c7038b07eae95af0741a3c9727b87c0ea88fcca5
koushith <koushith97@gmail.com>
2020-05-29 23:49:41 +0530

    call

---
+++ oops/debouncing and throttling/complex.js
@@ -0,0 +1,49 @@
+// // call apply and bind
+
+// var obj = {
+//   num: 2,
+// };
+
+// var addToThis = function (a) {
+//   return this.num + a;
+// };
+
+// var output = addToThis.call(obj, 3);
+// console.log(output);
+
+// let name = {
+//   first: 'koushith',
+//   last: 'Amin',
+// };
+
+// let name2 = {
+//   first: 'preetham',
+//   last: 'amin',
+// };
+
+// let fullname = function (first, last) {
+//   console.log(this.first + ' ' + this.last);
+// };
+
+// fullname.call(name2);
+// fullname.apply(name2, ['anki', 'salian']);
+
+const john = {
+  name: 'john',
+  age: 23,
+};
+
+const jane = {
+  name: 'jane',
+  age: 23,
+};
+
+function greeting(lang) {
+  console.log(
+    `hi, my name is ${this.name} and i am ${this.age} years old and i speak ${lang}`
+  );
+}
+
+const greetingJohn = greeting.bind(jane, 'eng');
+console.log(greeting);
+greetingJohn();
