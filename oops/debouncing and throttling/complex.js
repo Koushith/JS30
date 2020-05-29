561bf2afbbdab821c98d0b1e33cdc47b91315409
koushith <koushith97@gmail.com>
2020-05-30 00:11:02 +0530

    call apply and bind

--- oops/debouncing and throttling/complex.js
+++ oops/debouncing and throttling/complex.js
@@ -1,32 +1,56 @@
-// // call apply and bind
+// // // call apply and bind
 
-// var obj = {
-//   num: 2,
+// // var obj = {
+// //   num: 2,
+// // };
+
+// // var addToThis = function (a) {
+// //   return this.num + a;
+// // };
+
+// // var output = addToThis.call(obj, 3);
+// // console.log(output);
+
+// // let name = {
+// //   first: 'koushith',
+// //   last: 'Amin',
+// // };
+
+// // let name2 = {
+// //   first: 'preetham',
+// //   last: 'amin',
+// // };
+
+// // let fullname = function (first, last) {
+// //   console.log(this.first + ' ' + this.last);
+// // };
+
+// // fullname.call(name2);
+// // fullname.apply(name2, ['anki', 'salian']);
+
+// const john = {
+//   name: 'john',
+//   age: 23,
 // };
 
-// var addToThis = function (a) {
-//   return this.num + a;
+// const jane = {
+//   name: 'jane',
+//   age: 23,
 // };
 
-// var output = addToThis.call(obj, 3);
-// console.log(output);
+// function greeting(lang) {
+//   console.log(
+//     `hi, my name is ${this.name} and i am ${this.age} years old and i speak ${lang}`
+//   );
+// }
 
-// let name = {
-//   first: 'koushith',
-//   last: 'Amin',
-// };
+// // using bind
+// // const greetingJohn = greeting.bind(jane, 'eng');
+// // console.log(greeting);
+// // greetingJohn();
 
-// let name2 = {
-//   first: 'preetham',
-//   last: 'amin',
-// };
-
-// let fullname = function (first, last) {
-//   console.log(this.first + ' ' + this.last);
-// };
-
-// fullname.call(name2);
-// fullname.apply(name2, ['anki', 'salian']);
+// // using call
+// greeting.call(jane, 'english');
 
 const john = {
   name: 'john',
@@ -38,12 +62,10 @@ const jane = {
   age: 23,
 };
 
-function greeting(lang) {
-  console.log(
-    `hi, my name is ${this.name} and i am ${this.age} years old and i speak ${lang}`
-  );
+function greeting() {
+  console.log(` hi my name is ${this.name} and i am ${this.age} years old`);
 }
-
-const greetingJohn = greeting.bind(jane, 'eng');
-console.log(greeting);
-greetingJohn();
+// call
+//greeting.apply(jane, ['hi', 'koushith']);
+x = greeting.bind(john);
+x();
