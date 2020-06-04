c9ddbe7a5050c5a383574c82d2da02eae7c2b4c8
koushith <koushith97@gmail.com>
2020-06-05 00:34:15 +0530

    updated

--- oops/dom/script.js
+++ oops/dom/script.js
@@ -9,7 +9,28 @@
 // Array.from(wnf).forEach((ele) => console.log(ele));
 // console.log(wnf);
 
-var books = document.querySelectorAll('#book-list li .name');
+// var books = document.querySelectorAll('#book-list li .name');
 
-Array.from(books).forEach((ele) => (ele.textContent += '(Book Title)'));
-console.log(books);
+// Array.from(books).forEach((book) => (book.textContent += '(Book Title)'));
+// //console.log(books);
+
+// const booklist = document.querySelector('#book-list');
+// // booklist.innerHTML = '<h1>Changed now</h1>';
+// console.log(booklist.textContent);
+
+// node type
+
+// const banner = document.querySelector('#page-banner');
+
+// console.log('page-banner node type is:', banner.nodeName);
+// console.log(banner.nodeType);
+
+// const clonned = banner.cloneNode(true); //true clones everything inside div. orelse it will just clone the div
+// console.log(clonned);
+
+// move towards up -parent
+
+const booklist = document.querySelector('#book-list');
+
+// console.log('parent node is', booklist.parentNode);
+console.log('parent node is', booklist.parentNode);
