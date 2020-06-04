2b0607a0fdabdb6cd43b6297b35b58207b24075d
koushith <koushith97@gmail.com>
2020-06-04 23:41:59 +0530

    updated

--- oops/dom/script.js
+++ oops/dom/script.js
@@ -2,3 +2,14 @@
 // console.log(title);
 
 // Array.from(title).forEach((item) => console.log(item));
+
+// const wnf = document.querySelectorAll('#book-list li.name ');
+// console.log(wnf);
+
+// Array.from(wnf).forEach((ele) => console.log(ele));
+// console.log(wnf);
+
+var books = document.querySelectorAll('#book-list li .name');
+
+Array.from(books).forEach((ele) => (ele.textContent += '(Book Title)'));
+console.log(books);
