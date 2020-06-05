60db2fad24630e300800a4802f79b79123749577
koushith <koushith97@gmail.com>
2020-06-06 01:59:59 +0530

    12

--- oops/dom/script.js
+++ oops/dom/script.js
@@ -38,7 +38,7 @@
 // DOM EVENTS
 
 let btns = document.querySelectorAll('#book-list .delete');
-console.log(btns);
+// console.log(btns);
 Array.from(btns).forEach(function (btn) {
   btn.addEventListener('click', function (e) {
     console.log('Delete clicked');
@@ -50,3 +50,28 @@ Array.from(btns).forEach(function (btn) {
 });
 
 // form
+// Add books
+const list = document.querySelector('#book-list ul');
+const addForm = document.forms['add-book'];
+addForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+  //   grab the vaule of what user has typed. we are using attribute selector
+  const value = addForm.querySelector('input[type="text"]').value;
+  // console.log(value);
+
+  //  create elements acc to tags inside book ul
+  const li = document.createElement('li');
+  const bookName = document.createElement('span');
+  const deleteBtn = document.createElement('span');
+
+  //   add the text inside btn/span
+  deleteBtn.innerText = 'Delete';
+  // value from input field
+  bookName.innerText = value;
+
+  // append to document
+  li.appendChild(bookName);
+  li.appendChild(deleteBtn);
+  // append to ul
+  list.appendChild(li);
+});
